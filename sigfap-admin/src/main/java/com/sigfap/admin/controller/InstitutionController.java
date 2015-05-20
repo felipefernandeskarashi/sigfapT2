ae9bc6681e6ebcd3850a3551992606b2048af84a
Carlos Roberto <carlos@carlos-note.Workgroup>
2015-05-20 15:08:48 -0400

    Criado Métodos para Buscar e Exibir instituições

--- sigfap-admin/src/main/java/com/sigfap/admin/controller/InstitutionController.java
+++ sigfap-admin/src/main/java/com/sigfap/admin/controller/InstitutionController.java
@@ -1,15 +1,22 @@
 package com.sigfap.admin.controller;
 
+import java.util.Collection;
+
 import javax.inject.Inject;
 
-import com.sigfap.admin.model.dao.InstitutionDAO;
-import com.sigfap.admin.model.entity.Institution;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Example;
+import org.hibernate.criterion.Restrictions;
 
+import antlr.collections.List;
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.validator.Validator;
+
+import com.sigfap.admin.model.dao.InstitutionDAO;
+import com.sigfap.admin.model.entity.Institution;
 
 @Controller
 public class InstitutionController{
@@ -41,11 +48,6 @@ public class InstitutionController{
 	@Path("/registrar-instituicao1")
 	public void registrar(Institution institution){
 		try{
-			institution.setNome(institution.getNome());
-			institution.setSigla(institution.getSigla());
-			institution.setDependenciaAdm(institution.getDependenciaAdm());
-			institution.setIes(institution.getIes());
-			institution.setFinsLucrativos(institution.getFinsLucrativos());
 			institution.setAtiva(true);
 			dao.persist(institution);
 			
@@ -53,5 +55,17 @@ public class InstitutionController{
 			System.out.println(e.getMessage());
 		}
 		result.redirectTo(this).index();
+	}
+	
+	
+	@Path("/buscar-instituicao")
+	public void buscar(){
+		
+	}
+	
+	@Post
+	@Path("/exibir-instituicoes")
+	public void exibir(Institution institution, Integer tipoOrdenacao, Integer situacao, Integer limitePorPagina){
+		result.include("busca", dao.findByExample(institution));
 	}
 }
